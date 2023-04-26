88c906705267add4b129b33c33646ebf323f9229
mistamambo <mister.mambo@hotmail.com>
2023-04-26 14:20:31 +0200

    .

--- src/components/ProdCard.js
+++ src/components/ProdCard.js
@@ -1,7 +1,12 @@
+import pic from "./pic.png"
 export default function ProdCard () {
     return (
         <>
-        
+            <article>
+            <img src={pic}></img>
+            <h1>Title</h1>
+            <span>Action</span>
+            </article>
         </>
     )
 }
