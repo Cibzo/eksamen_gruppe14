0b9b2ee1cb97c04b7e7aae16c172c8b253e46491
Cibzo <Cibzo@outlook.com>
2023-05-02 17:26:52 +0200

    Fikset på antall prod

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,9 @@
-
-export default function Title ({text}) {
-    return <h1>{text}</h1>
+export default function Title ({text, bilde}) {
+    return (
+        <>
+        <h1>{text}</h1>
+        <img src={bilde}/>
+        </>
+    )
 }
 
