1705a204cbf85b16d37f501bd19def7a45fe4122
Cibzo <Cibzo@outlook.com>
2023-05-04 18:22:51 +0200

    .

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,19 @@
-export default function Nav(){
-    return (
-        <nav>
-            <button>Shop</button>
-            <button>My Games</button>
-            <button>Favourites</button>
-        </nav>
-    )
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <>
+      <nav>
+        <Link to="GameShop">
+          <button>Shop</button>
+        </Link>
+        <Link to="MyGames">
+          <button>My Games</button>
+        </Link>
+        <Link to="MyFavourites">
+          <button>Favourites</button>
+        </Link>
+      </nav>
+    </>
+  );
 }
