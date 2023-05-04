d49f3a3d1f5730c2a72642694d44cb6ab3ebd009
Jennie-Lovise Bjerke <jenniebjerke92@hotmail.com>
2023-05-04 20:24:13 +0200

    .

--- src/components/ProductDesc.js
+++ src/components/ProductDesc.js
@@ -1,14 +1,12 @@
 import Image from "./Image";
 import ProductTitle from "./ProductTitle";
 
-export default function ProductDesc({pic, name, sjanger}){
-    return (
-            <>
-            <ul>
-                <Image bilde={pic}/>
-                <ProductTitle prodName={name} />
-                <span className={sjanger}>{sjanger}</span>
-            </ul>
-            </>
-        )
+export default function ProductDesc({ pic, name, sjanger }) {
+  return (
+    <>
+      <Image bilde={pic} />
+      <ProductTitle prodName={name} />
+      <span className={sjanger}>{sjanger}</span>
+    </>
+  );
 }
