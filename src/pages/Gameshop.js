bc4e42927070a42bc91d34c30ecaad6ce5a670ec
mistamambo <mister.mambo@hotmail.com>
2023-05-04 19:07:45 +0200

    .

--- src/pages/Gameshop.js
+++ src/pages/Gameshop.js
@@ -1,5 +1,11 @@
 import PageContent from "../components/PageContent";
+import Header from "./MyFavourites";
 
 export default function GameShop({ props }) {
-  return <h1>Shop</h1>;
+  return (
+        <>
+        <h1>Shop</h1>;
+        <Header>hei</Header>
+        </>
+        )
 }
