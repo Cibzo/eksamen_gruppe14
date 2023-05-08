36a04caf6a663d17927414bc82ea02fd46a17d84
mistamambo <mister.mambo@hotmail.com>
2023-05-08 22:50:44 +0200

    fikset knappene visit. og buy knappen bruker Link.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import { Route, Routes } from "react-router";
 import "./App.css";
-import Header from "./components/Header";
-import shopLogo from "./data/logos/1x/macslogo_black.png"
-import Dashboard from "./pages/Dashboard";
-import Gameshop from "./pages/Gameshop";
-import MyGames from "./pages/MyGames";
-import MyFavourites from "./pages/MyFavourites";
+import RouterLink from "./components/RouterLink.js";
 
 function App() {
   return (
     <>
-      <Header logo={shopLogo} />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/GameShop" element={<Gameshop />} />
-        <Route path="/MyGames" element={<MyGames />} />
-        <Route path="/MyFavourites" element={<MyFavourites />} />
-      </Routes>
+    <RouterLink/>
     </>
   );
 }
---
+++ src/components/ExternalLink.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ExternalLink() {
+  return (
+    <div>
+      <p>Du blir nå omdirigert til en ekstern nettside.</p>
+    </div>
+  );
+}
+
+export default ExternalLink;
