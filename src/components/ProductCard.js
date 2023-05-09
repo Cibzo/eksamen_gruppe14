2e231ab82b1d6fc839e97d2566f44482e3786846
Jennie-Lovise Bjerke <jenniebjerke92@hotmail.com>
2023-05-09 19:03:32 +0200

    css

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,8 +1,12 @@
 import ProductDesc from "./ProductDesc";
-export default function ProductCard({id, bilde, title, generes}){
-    return <ProductDesc 
-                        key={id}
-                        pic={bilde}
-                        name={title}
-                        sjanger={generes.join(", ")}/>
+export default function ProductCard({ id, bilde, title, generes }) {
+  return (
+    <ProductDesc
+      key={id}
+      className={"game"}
+      pic={bilde}
+      name={title}
+      sjanger={generes.join(", ")}
+    />
+  );
 }
