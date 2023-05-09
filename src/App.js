1d7375e65172286c4948235f32bdf96fcba031cf
mistamambo <mister.mambo@hotmail.com>
2023-05-09 19:44:27 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import shopLogo from "./data/logos/1x/macslogo_black.png"
 import "./App.css";
+import Header from "./components/Header";
 import RouterLink from "./components/RouterLink.js";
+import { pages } from "./data/pages";
 
 function App() {
   return (
     <>
+    <Header logo={shopLogo} pgdata={pages} />
     <RouterLink/>
     </>
   );
--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,6 +1,4 @@
 import { pages } from "../data/pages";
-import shopLogo from "../data/logos/1x/macslogo_black.png"
-import Header from "./Header";
 import PageSection from "./PageSection";
 import Titles from "./Titles";
 
@@ -8,7 +6,7 @@ export default function PageContent({PageName, ClasName, TitleTag, kilde}){
     const pageData = kilde === "DashCont" ? pages : pages.filter(filterdPage => filterdPage.name === PageName)
     return (
         <>
-        <Header logo={shopLogo} pgdata={pages} />
+        
         <Titles titleName={PageName} titleTag={TitleTag}/>
         <main className={ClasName}>
         {pageData.map(section => (
