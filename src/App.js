f05c6df75dacfa423d13e31df08eeedbd7ba37db
mistamambo <mister.mambo@hotmail.com>
2023-05-15 20:46:23 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import "./App.css";
 import "./index.css"
-import RouterLink from "./RouterLink";
-
-
-
+import Dashboard from "./pages/Dashboard";
+import GameShop from "./pages/GameShop";
+import MyGames from "./pages/MyGames";
+import MyFavourites from "./pages/MyFavourites";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import GamePage from "./pages/GamePage";
 
 
 function App() {
   return (
     <>
-     
-      <RouterLink/>
+      <Routes id={150} >
+        <Route element={<Layout/>} >
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/GameShop" element={<GameShop />} />
+          <Route path="/MyGames" element={<MyGames />} />
+          <Route path="/MyFavourites" element={<MyFavourites />} />
+          <Route path="/:gameId" element={<GamePage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
