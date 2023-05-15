09686c25d30d8ca926de91dfa23cc6cdb34e2a9d
Jennie-Lovise Bjerke <jenniebjerke92@hotmail.com>
2023-05-15 19:39:42 +0200

    routerlink flyttet inn i App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
-import "./index.css"
-import RouterLink from "./RouterLink";
+import "./index.css";
+
 import Header from "./components/Header";
-import shopLogo from "./data/logos/1x/macslogo_black.png"
+import shopLogo from "./data/logos/1x/macslogo_black.png";
 import { pages } from "./data/pages";
-
-
-
+import Dashboard from "./pages/Dashboard";
+import GameShop from "./pages/GameShop";
+import MyGames from "./pages/MyGames";
+import MyFavourites from "./pages/MyFavourites";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header logo={shopLogo} pgdata={pages}/>
-      <RouterLink/>
+      <Header logo={shopLogo} pgdata={pages} />
+      <Routes id={150}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/GameShop" element={<GameShop />} />
+        <Route path="/MyGames" element={<MyGames />} />
+        <Route path="/MyFavourites" element={<MyFavourites />} />
+      </Routes>
     </>
   );
 }
