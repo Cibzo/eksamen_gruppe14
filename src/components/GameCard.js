60587a71ecc811a559c698ce52a0beb9e6e6ca0e
Jennie-Lovise Bjerke <jenniebjerke92@hotmail.com>
2023-05-15 22:04:41 +0200

    css edit endelig kravD

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -2,17 +2,31 @@ import { Link } from "react-router-dom";
 import Image from "./Image";
 import LinkBtn from "./LinkBtn";
 import Titles from "./Titles";
-export default function GameCard({bilde, titel, generes, clsname, id, lnk, slug, Bol}){
-    return (            
-            <article className={"gameCard" + clsname}>
-            <Image bilde={bilde} Img={"imgGame"} />
-            <Titles titleTag={"h3"} titleName={titel} />
-            <p className={"besk_" + clsname}>{generes.join(", ")}</p>
-            <LinkBtn atr={id} 
-                    lnk={lnk}
-                    cls={"BuyBtn" + clsname}
-                    name={"Buy"} hide={Bol} />
-                <Link to={`/${id}`}><button>Read more</button></Link>
-            </article> 
-        )
+export default function GameCard({
+  bilde,
+  titel,
+  generes,
+  clsname,
+  id,
+  lnk,
+  slug,
+  Bol,
+}) {
+  return (
+    <article className={"gameCard" + clsname}>
+      <Image bilde={bilde} Img={"imgGame"} />
+      <Titles titleTag={"h3"} titleName={titel} />
+      <p className={"besk_" + clsname}>{generes.join(", ")}</p>
+      <LinkBtn
+        atr={id}
+        lnk={lnk}
+        cls={"BuyBtn" + clsname}
+        name={"Buy"}
+        hide={Bol}
+      />
+      <Link to={`/${id}`}>
+        <button className="ReadMore">Read more</button>
+      </Link>
+    </article>
+  );
 }
