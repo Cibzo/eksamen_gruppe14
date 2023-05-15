54c1e7b08d8c2f597d83cc3c2757f887c3b184ef
Jennie-Lovise Bjerke <jenniebjerke92@hotmail.com>
2023-05-15 21:42:40 +0200

    css singlepage

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -5,30 +5,30 @@ import Titles from "../components/Titles";
 import LinkBtn from "../components/LinkBtn";
 
 export default function GamePage() {
-    const {gameId} = useParams()
-    const games = [...store, ...mygames]
-    const game = games.find((prod) => prod.id == gameId)
-    const { title, id, released, genres, link, img  } = game
-    let hidBuyBtn =  gameId > 5 ? true : false
+  const { gameId } = useParams();
+  const games = [...store, ...mygames];
+  const game = games.find((prod) => prod.id == gameId);
+  const { title, id, released, genres, link, img } = game;
+  let hidBuyBtn = gameId > 5 ? true : false;
 
-    return  (
-                <section>
-                    <article className={"gameCard"}>
-                    <Image bilde={img} ImgCls={"imgGame"} />
-                    <Titles className={"ImgGameTitl"} titleTag={"h3"} titleName={title} />
-                    <p className={"besk"}>Genres: {genres.join(", ")}</p>
-                    <time dateTime={released}>Published: {released}</time>
-                    <br/>
-                    <LinkBtn atr={id} 
-                            lnk={link}
-                            cls={"BuyBtn"}
-                            name={"Buy"} hide={hidBuyBtn} />
-                    </article>
-                </section>
-        
-            )
-
-                            
-    
-    
+  return (
+    <section className="singlePage">
+      <article className={"gameCard"}>
+        <Image bilde={img} ImgCls={"imgGame"} />
+        <div className="divSinglePage">
+          <Titles className={"ImgGameTitl"} titleTag={"h3"} titleName={title} />
+          <p className={"besk"}>Genres: {genres.join(", ")}</p>
+          <time dateTime={released}>Published: {released}</time>
+          <br />
+          <LinkBtn
+            atr={id}
+            lnk={link}
+            cls={"ButtonSinglePage"}
+            name={"Buy"}
+            hide={hidBuyBtn}
+          />
+        </div>
+      </article>
+    </section>
+  );
 }
